cf2e6f7aec7f03a6e849787a0137dcd84f8ff9d0
soren435 <sh637@hotmail.com>
2025-01-05 19:08:15 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'eu-north-1_kG9N5JpL5', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '444b3vrnftekbquo9b83ah4pvf', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '5kdr3s99ld1ti9ds9hpagfufq9', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'Europe(Stockholm)' // e.g. us-east-2
     },
     api: {
